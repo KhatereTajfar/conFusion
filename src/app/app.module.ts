ea813957e7a7148b9fd2aee333236915a4269fb9
Khatere Tajfar <tajfar.khatere@yahoo.com>
2018-09-10 00:33:11 +0430

    Promise Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 import 'hammerjs';
 
@@ -50,6 +52,7 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
+    MatProgressSpinnerModule,
     MatSelectModule,
     MatSlideToggleModule,
     MatFormFieldModule,
